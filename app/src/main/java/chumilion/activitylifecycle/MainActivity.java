3c3aedb0ac0c6b8eca9dccac8840e53d92b1dbaf
Chumilion <2016esison@tjhsst.edu>
2015-10-26 15:07:22 -0400

    almost sharedpreferences

--- app/src/main/java/chumilion/activitylifecycle/MainActivity.java
+++ app/src/main/java/chumilion/activitylifecycle/MainActivity.java
@@ -5,20 +5,35 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.logging.Handler;
 
 public class MainActivity extends AppCompatActivity
 {
-    private TextView onCreateC;
-    private TextView onStartC;
-    private TextView onResumeC;
-    private TextView onPauseC;
+    TextView onCreateC;
+    TextView onStartC;
+    TextView onResumeC;
+    TextView onPauseC;
+    TextView onStopC;
+    TextView onRestartC;
+    TextView onDestroyC;
+    int onCreateint = 0;
+    int onStartint = 0;
+    int onResumeint = 0;
+    int onPauseint = 0;
+    int onStopint = 0;
+    int onRestartint = 0;
+    int onDestroyint = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -26,28 +41,73 @@ public class MainActivity extends AppCompatActivity
         onStartC = (TextView) findViewById(R.id.onStartCount);
         onResumeC = (TextView) findViewById(R.id.onResumeCount);
         onPauseC = (TextView) findViewById(R.id.onPauseCount);
+        onStopC = (TextView) findViewById(R.id.onStopCount);
+        onRestartC = (TextView) findViewById(R.id.onRestartCount);
+        onDestroyC = (TextView) findViewById(R.id.onDestroyCount);
 
-        onCreateC.setText((Integer.parseInt(onCreateC.getText() + "") + 1) + "");
+        onCreateint++;
+        Log.i("onCreate", onCreateint + "");
+        Log.i("onCreate", onCreateC + "");
+        onCreateC.setText(onCreateint + "");
+        onStartC.setText(onStartint + "");
+        onResumeC.setText(onResumeint + "");
+        onPauseC.setText(onPauseint + "");
+        onStopC.setText(onStopint + "");
+        onRestartC.setText(onRestartint + "");
+        onDestroyC.setText(onDestroyint + "");
+
 
     }
     protected void onStart() {
         super.onStart();
-        setContentView(R.layout.activity_main);
 
-        onStartC.setText((Integer.parseInt(onStartC.getText() + "") + 1) + "");
+        onStartint++;
+        Log.i("onStart", onStartint + "");
+
+        onStartC.setText(onStartint + "");
     }
     protected void onResume() {
         super.onResume();
-        setContentView(R.layout.activity_main);
 
-        onResumeC.setText((Integer.parseInt(onResumeC.getText() + "") + 1) + "");
+        onResumeint++;
+        Log.i("onResume", onResumeint + "");
+
+        onResumeC.setText(onResumeint + "");
     }
 
     protected void onPause() {
         super.onPause();
-        setContentView(R.layout.activity_main);
 
-        onPauseC.setText((Integer.parseInt(onPauseC.getText() + "") + 1) + "");
+        onPauseint++;
+        Log.i("onPause", onPauseint + "");
+
+        onPauseC.setText(onPauseint + "");
+    }
+
+    protected void onStop() {
+        super.onStart();
+
+        onStopint++;
+        Log.i("onStart", onStopint + "");
+
+        onStartC.setText(onStopint + "");
+    }
+    protected void onRestart() {
+        super.onResume();
+
+        onRestartint++;
+        Log.i("onResume", onRestartint + "");
+
+        onResumeC.setText(onRestartint + "");
+    }
+
+    protected void onDestroy() {
+        super.onPause();
+
+        onDestroyint++;
+        Log.i("onPause", onDestroyint + "");
+
+        onPauseC.setText(onDestroyint + "");
     }
 
     @Override
